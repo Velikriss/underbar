947e6a52254b5b0fcf2ab898e30ea7fb26d956d8
nathan turinski <nathan turinski>
2016-12-24 21:01:25 +0000

    Part I. Reject using _.filter implemented

--- src/underbar.js
+++ src/underbar.js
@@ -95,11 +95,11 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    return _.filter(collection, function() {
+    return _.filter(collection, function(item) {
+    	if (test(item)) return false;
+    	return true;
 
-    
     });
-
   };
 
   // Produce a duplicate-free version of the array.
